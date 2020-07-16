85f169442015dc282063c169f12b49610dff379f
stone <335612970@qq.com>
2020-07-16 19:35:47 +0800

    设计模式-工厂模式 1.简单工厂、工厂方法、抽象工厂特例 2.抽象工厂

--- src/com/stone/parttern/factory/abstractfactory/ForceFactory.java
+++ src/com/stone/parttern/factory/abstractfactory/ForceFactory.java
@@ -1,5 +1,8 @@
 package com.stone.parttern.factory.abstractfactory;
 
+/**
+ * 抽象工厂：扩展产品族方便，扩展产品不方便
+ */
 public interface ForceFactory {
 
     Movable createMovable();
--- src/com/stone/parttern/factory/factorymethod/MovableFactory.java
+++ src/com/stone/parttern/factory/factorymethod/MovableFactory.java
@@ -1,5 +1,8 @@
 package com.stone.parttern.factory.factorymethod;
 
+/**
+ * 工厂方法：扩展产品方便，扩展产品族不方便
+ */
 public interface MovableFactory {
 
     Movable create();
